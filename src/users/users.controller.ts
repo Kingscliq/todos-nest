e8f9e84b0860a157d4c64d5747e21223ec7445c8
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-01-07 06:48:18 -0800

    Add: All Users Service and all users endpoin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,12 @@
 import { CreateUserDto } from './dtos/create.user.dto';
 import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Users } from './dtos/users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Get('sorted')
   getSingleSortedTodo(@Query('sortBy') sortBy: string): string {
     return `${sortBy}`;
@@ -11,10 +15,13 @@ export class UsersController {
   @Post('register')
   @UsePipes(new ValidationPipe()) // Add a new Instance of validation pipes for class validators to work
   createUser(@Body() createUserDto: CreateUserDto): string {
-    console.log(createUserDto);
     return `Created user ${createUserDto.username}`;
   }
 
+  @Get()
+  getAllUsers(): Users[] {
+    return this.userService.fetchUsers();
+  }
   @Get(':id')
   getSinglUser(@Param('id', ParseIntPipe) id: number): { id: number } {
     return { id };
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { username: 'Kings', email: 'kings@gmail.com' },
+    { username: 'paul', email: 'paul@gmail.com' },
+    { username: 'darl', email: 'darl@gmail.com' },
+  ];
+  fetchUsers = () => {
+    return this.users;
+  };
+}
